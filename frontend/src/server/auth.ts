0b8597528d14e1deb9d8e2d563ac28fd407cde35
Bobby Rösehag <bobbyem@gmail.com>
2023-02-09 16:43:09 +0100

    removed google auth provider

--- frontend/src/server/auth.ts
+++ frontend/src/server/auth.ts
@@ -5,7 +5,6 @@ import {
   type DefaultSession,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "../env/server.mjs";
 import { prisma } from "./db";
@@ -53,18 +52,7 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-      profile(profile) {
-        return {
-          // Return all the profile information you need.
-          // The only truly required field is `id`
-          // to be able identify the account when added to a database
-        };
-      },
-    }),
+    })
     /**
      * ...add more providers here
      *
